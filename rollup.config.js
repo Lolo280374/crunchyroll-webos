76187fbfb4ec8c58c666db47124a4359006056bd
Lolo280374 <56828143+Lolo280374@users.noreply.github.com>
2025-06-21 13:04:31 +0200

    Revert "try to fix rollup errors"
    This reverts commit a405b5faef0fde63dc63eeafa7bdcfdfa78925c8.

--- rollup.config.js
+++ rollup.config.js
@@ -2,8 +2,10 @@ export default {
     context: 'window',
     input: 'dist/scripts/main.js',
     output: {
-     file: 'dist/scripts/main.js',
-    format: 'es', // <--- THIS IS CRUCIAL
-     sourcemap: true
-}
+        compact: true,
+        file: 'dist/scripts/main.js',
+        format: 'iife',
+        inlineDynamicImports: true,
+        sourcemap: true,
+    }
 }
